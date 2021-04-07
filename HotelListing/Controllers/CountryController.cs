734faf4f0b391f516399da49ee867d0ffe3669b2
Alberto Martinez <j.alberto.martinez.d@gmail.com>
2021-04-07 15:16:22 -0500

    Added Country and Hotel methods, also fixed Unit of Work DI reference.

---
+++ HotelListing/Controllers/CountryController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using HotelListing.Contracts;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCountries()
+        {
+            var location = GetControllerActionNames();
+
+            try
+            {
+                var countries = await _unitOfWork.Countries.GetAll();
+                var result = _mapper.Map<IList<CountryDTO>>(countries);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return InternalError($"{location}: Error", ex);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            var location = GetControllerActionNames();
+
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(c => c.Id == id, new List<string> { "Hotels" });
+                var result = _mapper.Map<CountryDTO>(country);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return InternalError($"{location}: Error", ex);
+            }
+        }
+
+        private ObjectResult InternalError(string message, Exception ex = null)
+        {
+            if (ex != null)
+            {
+                _logger.LogError(ex, message);
+                return StatusCode(500, ex.Message);
+            }
+            else
+            {
+                _logger.LogError(message);
+                return StatusCode(500, message);
+            }
+        }
+
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+
+            return $"{controller} - {action}";
+        }
+
+    }
+}
---
+++ HotelListing/Controllers/HotelController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using HotelListing.Contracts;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<HotelController> _logger;
+        private readonly IMapper _mapper;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotels()
+        {
+            var location = GetControllerActionNames();
+
+            try
+            {
+                var hotels = await _unitOfWork.Hotels.GetAll();
+                var result = _mapper.Map<IList<HotelDTO>>(hotels);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return InternalError($"{location}: Error", ex);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotel(int id)
+        {
+            var location = GetControllerActionNames();
+
+            try
+            {
+                var hotel = await _unitOfWork.Hotels.Get(c => c.Id == id, new List<string> { "Country" });
+                var result = _mapper.Map<HotelDTO>(hotel);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return InternalError($"{location}: Error", ex);
+            }
+        }
+
+        private ObjectResult InternalError(string message, Exception ex = null)
+        {
+            if (ex != null)
+            {
+                _logger.LogError(ex, message);
+                return StatusCode(500, ex.Message);
+            }
+            else
+            {
+                _logger.LogError(message);
+                return StatusCode(500, message);
+            }
+        }
+
+        private string GetControllerActionNames()
+        {
+            var controller = ControllerContext.ActionDescriptor.ControllerName;
+            var action = ControllerContext.ActionDescriptor.ActionName;
+
+            return $"{controller} - {action}";
+        }
+    }
+}
